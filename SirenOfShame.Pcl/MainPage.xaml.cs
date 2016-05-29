d0cc582b2cfdeb6d6378223c5504647269d08593
Lee Richardson <github@leerichardson.com>
2016-05-28 21:35:49 -0400

    Can connect to an sos device

--- SirenOfShame.Pcl/MainPage.xaml.cs
+++ SirenOfShame.Pcl/MainPage.xaml.cs
@@ -1,30 +1,77 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Devices.HumanInterfaceDevice;
+using Windows.Storage;
 using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace SirenOfShame.Pcl
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class MainPage
     {
+        private static ushort UsageId = 1;
+        private static ushort UsagePage = 0xFF9C;
+        private const ushort VendorId = 0x16d0;
+        private const ushort ProductId = 0x0646;
+
         public MainPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+            Loaded += OnLoaded;
+        }
+
+        private async void OnLoaded(object sender, RoutedEventArgs routedEventArgs)
+        {
+            var sosDevice = await GetDeviceInfo();
+            using (HidDevice hidDevice = await HidDevice.FromIdAsync(sosDevice.Id, FileAccessMode.ReadWrite))
+            {
+                if (hidDevice != null)
+                {
+                    System.Diagnostics.Debug.WriteLine(hidDevice.UsageId);
+                    System.Diagnostics.Debug.WriteLine(hidDevice.UsagePage);
+                    System.Diagnostics.Debug.WriteLine(hidDevice.ProductId);
+                    System.Diagnostics.Debug.WriteLine(hidDevice.VendorId);
+                }
+                else
+                {
+                    var deviceAccessStatus = DeviceAccessInformation.CreateFromId(sosDevice.Id).CurrentStatus;
+
+                    string notificationMessage;
+                    if (deviceAccessStatus == DeviceAccessStatus.DeniedByUser)
+                    {
+                        notificationMessage = "Access to the device was blocked by the user : " + sosDevice.Id;
+                    }
+                    else if (deviceAccessStatus == DeviceAccessStatus.DeniedBySystem)
+                    {
+                        // This status is most likely caused by app permissions (did not declare the device in the app's package.appxmanifest)
+                        // This status does not cover the case where the device is already opened by another app.
+                        notificationMessage = "Access to the device was blocked by the system : " + sosDevice.Id;
+                    }
+                    else
+                    {
+                        // Most likely the device is opened by another app, but cannot be sure
+                        notificationMessage = "Unknown error, possibly opened by another app : " + sosDevice.Id;
+                    }
+                    System.Diagnostics.Debug.WriteLine(notificationMessage);
+                }
+            }
+        }
+
+        private static async Task<DeviceInformation> GetDeviceInfo()
+        {
+            var deviceSelector = GetDeviceSelector();
+            var devices = await DeviceInformation.FindAllAsync(deviceSelector);
+            return devices.AsEnumerable().FirstOrDefault();
+        }
+
+        private static string GetDeviceSelector()
+        {
+            var deviceSelector = HidDevice.GetDeviceSelector(UsagePage, UsageId, VendorId, ProductId);
+            return deviceSelector;
         }
     }
 }
